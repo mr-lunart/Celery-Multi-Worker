0b745a827b8e13a91a9f266386010b45f831045e
mr-lunart <dewanata_85@yahoo.com>
2025-12-15 12:56:46 +0700

    update redis hostname

--- scrapper.py
+++ scrapper.py
@@ -19,8 +19,8 @@ aws_client = boto3.client(
 )
 
 app = Celery("scrapper")
-app.conf.broker_url = 'redis://localhost:6379/0'
-app.conf.result_backend = 'redis://localhost:6379/0'
+app.conf.broker_url = 'redis://127.0.0.1:6379/0'
+app.conf.result_backend = 'redis://127.0.0.1:6379/0'
 app.conf.task_routes = {
     'tasks.consumer_sqs': {'queue': 'consumer_sqs'},
     'tasks.delete_sqs': {'queue': 'consumer_sqs'}
